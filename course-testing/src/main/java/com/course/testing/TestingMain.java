4da944a78211ae0fd0e18ca9606cbfe4e72a6c05
sionziom <sionziom@gmail.com>
2020-03-29 12:23:52 +0200

    first commit

--- course-testing/src/main/java/com/course/testing/TestingMain.java
+++ course-testing/src/main/java/com/course/testing/TestingMain.java
@@ -1,8 +1,29 @@
 package com.course.testing;
 
+import com.course.testing.calculator.Calculator;
+
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+
+        int result1 = calculator.add(2,3);
+
+        if (result1 == 5) {
+            System.out.println("test1 OK");
+        } else {
+            System.out.println("error");
+        }
+
+        int result2 = calculator.subtract(10, 4);
+
+        if(result2 == 6){
+            System.out.println("test2 OK");
+        }
+        else {
+            System.out.println("error");
+        }
 
     }
 }
---
+++ course-testing/src/main/java/com/course/testing/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.course.testing.calculator;
+
+public class Calculator {
+
+    public int add(int A, int B){
+        return A+B;
+    }
+    public int subtract(int A, int B){
+        return A-B;
+    }
+}

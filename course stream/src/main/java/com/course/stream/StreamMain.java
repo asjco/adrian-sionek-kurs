0bd886a824151f6350dbe78a58a35a0f4b65a503
sionziom <sionziom@gmail.com>
2020-04-08 20:42:07 +0200

    immutable ForumUser

--- course stream/src/main/java/com/course/stream/StreamMain.java
+++ course stream/src/main/java/com/course/stream/StreamMain.java
@@ -1,20 +1,16 @@
 package com.course.stream;
 
 import com.course.stream.beautifier.PoemBeautifier;
+import com.course.stream.iterate.NumbersGenerator;
 import com.course.stream.lambda.*;
 import com.course.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
 
     public static void main(String[] args){
-        System.out.println("Welcome to module 7 - Stream ");
+        System.out.println("Using stream to generate numbers from 1 to 20 ");
+        NumbersGenerator.generateEven(20);
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-
-        poemBeautifier.beautify("example 1", string -> "ABC " + string + " ABC");
-        poemBeautifier.beautify("example 2", string -> string.toUpperCase());
-        poemBeautifier.beautify("example 3", string -> "*** "+string+" ***");
-        poemBeautifier.beautify("example 4", string -> "<3 "+string+" <3");
     }
 
 

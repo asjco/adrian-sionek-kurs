d9fc55870b30836fbcc2784f28f162c2e6ee1aad
sionziom <sionziom@gmail.com>
2020-04-23 23:19:52 +0200

    titles iteration

---
+++ course-good-patterns/src/main/java/com/course/good/patterns/challenges/MovieStoreMain.java
@@ -0,0 +1,12 @@
+package com.course.good.patterns.challenges;
+
+public class MovieStoreMain {
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+
+        System.out.println(movieStore.getTitles());
+
+
+    }
+}

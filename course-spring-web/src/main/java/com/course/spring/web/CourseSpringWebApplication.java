e0e1b91751496dede0c6fbe1cc5efdce183add55
sionziom <sionziom@gmail.com>
2020-05-02 12:09:49 +0200

    Spring Web

---
+++ course-spring-web/src/main/java/com/course/spring/web/CourseSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.course.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSpringWebApplication.class, args);
+    }
+
+}
---
+++ course-spring-web/src/test/java/com/course/spring/web/CourseSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

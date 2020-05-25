4ca870357fedbdc578665b9cdb3674bac26ffc4a
sionziom <sionziom@gmail.com>
2020-05-25 21:30:27 +0200

    hibernate

---
+++ course-hibernate/src/main/java/com/course/hibernate/CourseHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.course.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseHibernateApplication.class, args);
+    }
+}
